b4a49b2eb12bb33eb2c7bb96071fee417ef371f0
Robby Spampneto <rspampneto@gmail.com>
2023-04-11 11:34:32 -0500

    change port # backend runs on

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import { ExampleController } from './example/example.controller';
+import { ExampleModule } from './example/example.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule, 
+    ExampleModule,
+    ConfigModule.forRoot(),
+  ],
   controllers: [],
   providers: [],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.enableCors({origin: '*'});
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
