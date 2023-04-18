3c22391eac518c7d5ff04289b604bc64f2577dd7
Otis Jackson IV <ojacks7@lsu.edu>
2023-04-18 12:36:32 -0500

    Updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
-import Example from './components/example.tsx';
+import "./App.css";
+import Example from "./components/example.tsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Welcome } from "./pages/welcome_page/Welcome";
+import { Select } from "./pages/concentration_page/Select";
+import { AppBar } from "./components/application_bar/AppBar";
 
 function App() {
   return (
     <div className="App">
-      <Example></Example>
+      <Router>
+        <AppBar />
+
+        <Routes>
+          <Route path="/" element={<Welcome />}></Route>
+          <Route path="/select" element={<Select />}></Route>
+          <Route></Route>
+        </Routes>
+      </Router>
+      {/* <Example></Example> */}
     </div>
   );
 }
