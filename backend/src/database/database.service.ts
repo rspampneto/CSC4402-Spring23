5c276bb0fdb61243c96d2f7870954bbf9478bfe8
RaviStimphil <97135286+RaviStimphil@users.noreply.github.com>
2023-04-20 13:16:17 -0500

    Table Update (Still Broken)

--- backend/src/database/database.service.ts
+++ backend/src/database/database.service.ts
@@ -34,13 +34,11 @@ export class DatabaseService {
     }
 
     try{
-      sql = 'CREATE TABLE course (id varchar(10), concentration varchar(20), title varchar(20), primary key (id, concentration));';
+      sql = 'CREATE TABLE course (course_id varchar(10), concentration varchar(20), title varchar(20), primary key (course_id, concentration));';
       await this.connection.query(sql);
-      sql = 'CREATE TABLE course (id varchar(10), course_id varchar(10), semester varchar(10), start varchar(10), end varchar(10), days varchar(20), room varchar(20), prof varchar(30), primary key(id, course_id, semester), foreign key(course_id) references course);';
+      sql = 'CREATE TABLE section (id varchar(10), course_id varchar(10), semester varchar(10), start varchar(10), end varchar(10), days varchar(20), room varchar(20), prof varchar(30), primary key(id, course_id, semester), foreign key(course_id) references course);';
       await this.connection.query(sql);
-      sql = 'CREATE TABLE course (id varchar(10), title varchar(20), primary key(id));';
-      await this.connection.query(sql);
-      sql = "insert into course values ('1350','Intro. to CompSci','4');";
+      sql = 'CREATE TABLE concentration (id varchar(10), title varchar(20), primary key(id));';
       await this.connection.query(sql);
     } catch(e){
       console.log("Error creating csc4402db. " + e);
