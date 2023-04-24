8793001cef152b454d7550d126dc42fcf3e08277
Otis Jackson IV <ojacks7@lsu.edu>
2023-04-24 09:16:05 -0500

    Timeline Components Update

--- frontend/src/pages/timeline_pages/t_components/CoreContainer.js
+++ frontend/src/pages/timeline_pages/t_components/CoreContainer.js
@@ -1,10 +1,24 @@
 import react from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, Stack, HStack, Text } from "@chakra-ui/react";
+import "./comp.css";
 
 const CoreContainer = () => {
   // Attributes
+
   // Functions
   // JSX
-  return <Box></Box>;
+  return (
+    <Stack id="core-container">
+      <Box id="core-title">
+        <Text>Core Concentrated Courses</Text>
+      </Box>
+
+      {/* Scroll Container */}
+      <HStack id="scroll-container">
+        {/* Semester List */}
+        <HStack id="semester-list"></HStack>
+      </HStack>
+    </Stack>
+  );
 };
 export default CoreContainer;
--- frontend/src/pages/timeline_pages/t_components/ElectiveContainer.js
+++ frontend/src/pages/timeline_pages/t_components/ElectiveContainer.js
@@ -1,10 +1,17 @@
 import react from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, Stack } from "@chakra-ui/react";
+import "./comp.css";
 
 const ElectiveContainer = () => {
   // Attributes
   // Functions
   // JSX
-  return <Box></Box>;
+  return (
+    // Elective Container
+    <Stack id="elective-container">
+      {/* Elective List */}
+      <Stack id="elective-list"></Stack>
+    </Stack>
+  );
 };
 export default ElectiveContainer;
